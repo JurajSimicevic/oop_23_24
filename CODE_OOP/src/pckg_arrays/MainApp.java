7061b98ca59a381bcf4d99b31be6f38e3f0ad52b
JurajSimicevic <jsimicevi22@unizd.hr>
2024-03-18 19:30:48 +0100

    init commit for arrays

--- CODE_OOP/src/pckg_arrays/MainApp.java
+++ CODE_OOP/src/pckg_arrays/MainApp.java
@@ -1,27 +1,33 @@
 package pckg_arrays;
 
+import java.util.Scanner;
+
 public class MainApp {
 
     private static int value;
     private static String name;
     private static String[] names = new String[5];
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        float[] ints = new float[10];
-        int[] newInts = {10, 30, -10, 140, 34, 89, 54};
-        System.out.println("First array: " + ints.length);
-        System.out.println("Snd array: " + newInts.length);
-        System.out.println("Third element - fst: " + ints[3]);
-        System.out.println("Third element - snd: " + newInts[3]);
-        for(int k=0; k < ints.length; k++){
-            ints[k] = k+1;
-        }
-        System.out.println("Third element - fst: " + ints[3]);
-        ints[3] = 400.5f;
-        System.out.println("Third element - fst: " + ints[3]);
-//        ints[10] = 1000;
-        changeElement(names, 2);
-        System.out.println(names[2]);
+//        float[] ints = new float[10];
+//        int[] newInts = {10, 30, -10, 140, 34, 89, 54};
+//        System.out.println("First array: " + ints.length);
+//        System.out.println("Snd array: " + newInts.length);
+//        System.out.println("Third element - fst: " + ints[3]);
+//        System.out.println("Third element - snd: " + newInts[3]);
+//        for(int k=0; k < ints.length; k++){
+//            ints[k] = k+1;
+//        }
+//        System.out.println("Third element - fst: " + ints[3]);
+//        ints[3] = 400.5f;
+//        System.out.println("Third element - fst: " + ints[3]);
+////       ints[10] = 1000;
+//        changeElement(names, 2);
+//        System.out.println(names[2]);
+        populateStringNames(names);
+        listElements(names);
+
     }
 
     private static void changeElement(String[] nume, int indx){
@@ -34,4 +40,24 @@ public class MainApp {
         }
 
     }
+
+    private static void populateStringNames(String[] numeOpet){
+        int cnt = 0;
+        String name;
+        while(cnt < numeOpet.length){
+            System.out.println("Unesi ime:");
+            name = scanner.nextLine();
+            numeOpet[cnt] = name;
+            cnt++;
+        }
+        System.out.println("Finished!");
+    }
+
+    private static void listElements(String[] names){
+        System.out.println("-------------------------------------");
+        for(int k=0; k < names.length; k++){
+            System.out.println("Name: " + names[k]);
+        }
+    }
+
 }
